c325f191fc903915ebb0b72485c8898b81196da1
ilaydamul <Ilayda2003.1905@hotmail.com>
2024-01-15 09:22:38 +0300

    baslangic

---
+++ App.js
@@ -0,0 +1,30 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// SCREENS
+import { Home } from 'screens/Home.js';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <NavigationContainer>
+        <Stack name="Home" component={} />
+      </NavigationContainer>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
